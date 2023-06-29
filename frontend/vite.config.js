8680b17681482ad6f25fe3f855a6cdb09f4bcc80
Ganivada Mouli <vishnumouli0@gmail.com>
2023-06-29 21:37:19 +0530

    updated vite js config file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 1000000,
+    chunkSizeWarningLimit: 1600,
   },
 });
